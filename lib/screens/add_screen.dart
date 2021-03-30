da743db0f2b499fe34066e74fecd996df296aea5
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-30 10:01:56 +0900

    ノート追加フォーム作成

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -3,8 +3,54 @@ import 'package:flutter/material.dart';
 class AddScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    String titleText;
+    String descriptionText;
+
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(
+        title: Text('ノートアプリ'),
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'タイトル',
+                hintStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white)
+              ),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              onChanged: (val) {
+                titleText = val;
+              },
+            ),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '内容',
+                  hintStyle: TextStyle(fontSize: 16, color: Colors.white)
+                ),
+                style: TextStyle(fontSize: 16, color: Colors.white),
+                onChanged: (val) {
+                  descriptionText = val;
+                },
+              ),
+            ),
+            FlatButton(
+              onPressed: () {},
+              color: Colors.white,
+              child: Text('追加', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.lightBlue),),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
